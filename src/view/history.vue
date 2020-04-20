54fb9801a30cc3bbe8dba109cc522b391ceb4c39
ichvanul <ichvanulyp@gmail.com>
2020-04-20 09:32:11 +0700

    first-commit

---
+++ src/view/history.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <Navbar2/>
+  <div>
+    <Sidebar/>
+  </div>
+  <div>
+    <Rectangle/>
+  </div>
+</div>
+</template>
+
+<script>
+import Navbar2 from '../components/_module/Navbar2.vue';
+import Sidebar from '../components/_module/Sidebar.vue';
+import Rectangle from '../components/_module/Rectangle.vue';
+
+export default {
+  name: 'History',
+  components: {
+    Navbar2,
+    Sidebar,
+    Rectangle,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
